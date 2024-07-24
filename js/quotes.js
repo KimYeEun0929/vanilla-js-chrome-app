d29fc54c3135fd3a8be32464c304d38e1e97e640
nabi1110 <nabinabi1110@gmail.com>
2024-07-24 20:49:28 +0900

    ~weather

--- js/quotes.js
+++ js/quotes.js
@@ -1,27 +1,27 @@
 const quotes = [
   {
     quote: '명랑하게 살자! 명랑한 사람이 결국 이긴다!',
-    author: '최화정',
+    author: '-최화정',
   },
   {
     quote: '커피에 빵을 곁들이지 않으면 그건 현대인이 아니다.',
-    author: '최화정',
+    author: '-최화정',
   },
   {
     quote: '안되면 말고',
-    author: '박명수',
+    author: '-박명수',
   },
   {
     quote: '돈 드는 거 하지 마라',
-    author: '박명수',
+    author: '-박명수',
   },
   {
     quote: '되면 한다.',
-    author: '박명수',
+    author: '-박명수',
   },
   {
     quote: '허리 펴고 입꼬리 올리면 못 할 일이 없다고. 세상에',
-    author: '최화정',
+    author: '-최화정',
   },
 ];
 
