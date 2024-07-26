c6067f862a647f7acb26903fcee5603d55957fd7
nabi1110 <nabinabi1110@gmail.com>
2024-07-26 19:56:30 +0900

    css 완성

--- js/backgrounds.js
+++ js/backgrounds.js
@@ -6,4 +6,15 @@ const bgImage = document.createElement('img');
 
 bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+//bgImage.id = 'bg';
+
+//document.body.style.background(bgImage);
+
+//document.body.appendChild(bgImage);
+
+// 배경 이미지 설정
+document.body.style.backgroundImage = `url(${bgImage.src})`;
+document.body.style.backgroundRepeat = 'no-repeat';
+document.body.style.height = '100vh';
+document.body.style.width = '100vw';
+document.body.style.backgroundSize = 'cover'; // 배경 이미지 크기 조절 (선택 사항)
